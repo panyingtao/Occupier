d1759be65975fbc87ac0335fbfa50dc8f37d4d2e
5KU35IHWGD2YGJY\YingTaoPan <313800161@qq.com>
2018-10-10 18:23:27 +0800

    emmmmm,增加一部分的搜索代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,18 +8,21 @@ Page({
     keyWord: "",
   },
   onLoad: function (options) {
-    var list = [];
-    list = owners.dataList();
+    var list = owners.dataList();
     this.setData({
       dataList: list,
     });
     console.log(list);
   },
-  
+
   // 获得用户输入的关键字
   getKeyWord: function (e) {
     this.getData({
       keyWord: e.detail.value
     });
+  },
+  // 点击搜索事件
+  searchOccupier:function(e){
+    var title=this.data.keyWord;
   }
 })
