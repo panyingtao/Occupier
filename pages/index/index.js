bfc3b778c38050ce6c4327f635f97d2701dba32a
5KU35IHWGD2YGJY\YingTaoPan <313800161@qq.com>
2018-10-11 10:35:33 +0800

    完成首页页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,24 +5,45 @@ var owners = require("../../utils/owners.js");
 
 Page({
   data: {
-    keyWord: "",
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
     var list = owners.dataList();
     this.setData({
       dataList: list,
+      list: list
     });
     console.log(list);
   },
 
   // 获得用户输入的关键字
-  getKeyWord: function (e) {
-    this.getData({
-      keyWord: e.detail.value
+  getKeyWord: function(e) {
+    var that = this;
+    that.setData({
+      keyWord: e.detail.value,
     });
+    console.log("关键字" + that.data.keyWord);
   },
   // 点击搜索事件
-  searchOccupier:function(e){
-    var title=this.data.keyWord;
+  searchOccupier: function(e) {
+    var title = this.data.keyWord;
+    var list = this.data.list;
+    if (title != "") {
+      var owners = [];
+      for (var i in list) {
+        if (list[i].goods_title == title) {
+          owners.push(list[i]);
+          console.log(list[i]);
+        }
+      }
+      this.setData({
+        dataList: owners
+      });
+    } else {
+      this.setData({
+        dataList: list
+      });
+    }
+    console.log(this.data.dataList);
+
   }
 })
--- utils/owners.js
+++ utils/owners.js
@@ -5,22 +5,22 @@ function dataList(){
     goods_img: '../image/1.jpg',
     goods_site: '泰国'
   }, {
-    goods_id: 1,
+    goods_id: 2,
     goods_title: '东京',
     goods_img: '../image/1.jpg',
     goods_site: '日本'
   }, {
-    goods_id: 2,
+    goods_id: 3,
     goods_title: '伦敦',
     goods_img: '../image/1.jpg',
     goods_site: '英国'
   }, {
-    goods_id: 3,
+    goods_id: 4,
     goods_title: '巴黎',
     goods_img: '../image/1.jpg',
     goods_site: '法国'
   }, {
-    goods_id: 4,
+    goods_id: 5,
     goods_title: '柏林',
     goods_img: '../image/1.jpg',
     goods_site: '德国'
